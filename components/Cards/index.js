f07628abe1448c803751b0840a533439c614b77f
Taylor Wise <taylor2julie@gmail.com>
2020-03-20 22:27:16 -0500

    completed proeject.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardContainer = document.querySelector(".cards-container")
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    const responseInfo = response.data.articles
+
+    responseInfo.javascript.forEach(array => {
+        const newCard = createCard(array)
+        cardContainer.appendChild(newCard)    
+    });
+    responseInfo.bootstrap.forEach(array => {
+        const newCard = createCard(array)
+        cardContainer.appendChild(newCard)    
+    });
+    responseInfo.technology.forEach(array => {
+        const newCard = createCard(array)
+        cardContainer.appendChild(newCard)    
+    });
+    responseInfo.jquery.forEach(array => {
+            const newCard = createCard(array)
+            cardContainer.appendChild(newCard)    
+    });
+    responseInfo.node.forEach(array => {
+        const newCard = createCard(array)
+        cardContainer.appendChild(newCard)    
+    }); 
+})
+.catch (err => {
+    console.log(err)
+})
+
+const createCard = (obj) => {
+    const card = document.createElement("div")
+    const headliner = document.createElement("div")
+    const author = document.createElement("div")
+    const imgContainer = document.createElement("div")
+    const image = document.createElement("img")
+    const authName = document.createElement("span")
+
+    card.appendChild(headliner)
+    card.appendChild(author)
+    author.appendChild(imgContainer)    
+    imgContainer.appendChild(image)
+    author.appendChild(authName)
+
+    card.classList.add("card")
+    headliner.classList.add("headline")
+    author.classList.add("author")
+    imgContainer.classList.add("img-container")
+
+    headliner.textContent = obj.headline
+    image.src = obj.authorPhoto
+    authName.textContent = `By ${obj.authorName}`
+
+    return card
+}
+
+
